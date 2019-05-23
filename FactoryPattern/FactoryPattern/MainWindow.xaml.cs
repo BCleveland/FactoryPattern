8fce4bc9ccd47ecb88c4a91f3653ae9def6cf377
Tien Cu <TCu@student.neumont.edu>
2019-05-22 20:19:27 -0600

    Have the basic functionality for storing data to an observable object, displaying the on the list box through binding, and removing the most recent data from the listbox.

--- FactoryPattern/FactoryPattern/MainWindow.xaml.cs
+++ FactoryPattern/FactoryPattern/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FactoryPattern.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,55 @@ namespace FactoryPattern
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		ObservableCollection<WindowData> windowDatas = new ObservableCollection<WindowData>();
+		private int currIndex = -1;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private WindowData InputWindowData()
+		{
+			WindowData newData = new WindowData();
+			newData.ContentText = TextboxContent.Text;
+
+			int numberData;
+			Int32.TryParse(TextboxHeight.Text, out numberData);
+			newData.HeightLength = numberData;
+
+			Int32.TryParse(TextboxWidth.Text, out numberData);
+			newData.WidthLength = numberData;
+
+			Int32.TryParse(TextboxTop.Text, out numberData);
+			newData.TopPosition = numberData;
+
+			Int32.TryParse(TextboxLeft.Text, out numberData);
+			newData.LeftPosition = numberData;
+
+			return newData;
+		}
+
+		private void Add_Click(object sender, RoutedEventArgs e)
+		{
+			windowDatas.Add(InputWindowData());
+			currIndex++;
+			ListboxMain.ItemsSource = windowDatas;
+		}
+
+		private void Undo_Click(object sender, RoutedEventArgs e)
+		{
+			if (currIndex != -1)
+			{
+				windowDatas.RemoveAt(currIndex);
+				currIndex--;
+				ListboxMain.Items.Refresh();
+			}
+		}
+
+		private void Build_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
---
+++ FactoryPattern/FactoryPattern/Models/WindowData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Models
+{
+	class WindowData
+	{
+		public string ContentText { get; set; }
+		public int HeightLength { get; set; }
+		public int WidthLength { get; set; }
+		public int TopPosition { get; set; }
+		public int LeftPosition { get; set; }
+	}
+}
