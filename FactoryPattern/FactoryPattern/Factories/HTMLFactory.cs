df80043378a4a26741b9747c3c6157a647e2f86a
Brendan <btcleveland@gmail.com>
2019-05-24 08:33:57 -0600

    Fixed combo box

--- FactoryPattern/FactoryPattern/Factories/HTMLFactory.cs
+++ FactoryPattern/FactoryPattern/Factories/HTMLFactory.cs
@@ -1,6 +1,7 @@
 ﻿using FactoryPattern.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,15 @@ namespace FactoryPattern.Factories
     {
 		public void ExportToFile(List<WindowData> data)
 		{
-			throw new NotImplementedException();
+			StringBuilder code = new StringBuilder();
+			foreach(var element in data)
+			{
+				code.AppendLine("ctx.fillRect(0, 0, 150, 75);");
+			}
+			string file = File.OpenText("../../HtmlTemplate.html").ReadToEnd();
+			string finished = file.Replace("<!--CodeHere-->", code.ToString());
+			File.WriteAllText("Output.html", finished);
+			System.Diagnostics.Process.Start("Output.html");
 		}
     }
 }
