9fa0ddc3b9b8840aa7b50c2e6ec04df1e400c9f9
Tien Cu <TCu@student.neumont.edu>
2019-05-27 20:35:46 -0600

    Exported data to .xml format | once exported file will be in the bin folder

--- FactoryPattern/FactoryPattern/Factories/WPFFactory.cs
+++ FactoryPattern/FactoryPattern/Factories/WPFFactory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FactoryPattern.Factories
 {
@@ -12,7 +13,19 @@ namespace FactoryPattern.Factories
 		public void ExportToFile(List<WindowData> data)
 		{
 			//TODO: Make data open in a WPF window
-			throw new NotImplementedException();
+			XElement element = new XElement("Items");
+			foreach(WindowData item in data)
+			{
+				element.Add(new XElement("Type", item.ElementType));
+				element.Add(new XElement("Content", item.ContentText));
+				element.Add(new XElement("Height", item.HeightLength));
+				element.Add(new XElement("Width", item.WidthLength));
+				element.Add(new XElement("Top", item.TopPosition));
+				element.Add(new XElement("Left", item.LeftPosition));
+			}
+			XDocument document = new XDocument();
+			document.Add(element);
+			document.Save("items.xml", SaveOptions.DisableFormatting);
 		}
 	}
 }
